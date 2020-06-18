e12e7e7023875219240f8229dff981dea497fa99
gardhr <24468810+gardhr@users.noreply.github.com>
2020-06-18 10:28:43 -0500

    update

--- fields.cpp
+++ fields.cpp
@@ -29,6 +29,16 @@ int main(int argc, char** argv)
  cout << "Length over mass constant: " << Cr << endl;
  auto Lm = Pl * Pm;
  cout << "Length-mass constant: " << Lm << endl;
+/*
+  Artifact introduced by the definition of the ampere
+
+  https://en.wikipedia.org/wiki/Ampere#Former_definition_in_the_SI
+*/
+ auto ampere_scaling_factor = 1e-7;
+ auto Pc2_ = Lm / ampere_scaling_factor;
+ cout << "Length-mass (with scaling removed): " << Pc2_ << endl;
+ auto Pc2 = Pc * Pc;
+ cout << "Planck charge squared: " << Pc2 << " (Same as above!)" << endl;
  auto Cf = Lm / (Pt * Pt);
  cout << "Planck force: " << Cf << endl;
  auto Ce = Cf * Pl;
@@ -38,27 +48,44 @@ int main(int argc, char** argv)
  auto Pi = 4 * atan(double(1.0));
  auto Ch = 2 * Pi * Cb;
  cout << "Planck's constant: " << Ch << endl;
- cout << "*** Possible force field constants ***" << endl;
- auto F0 = Cv2; // ie: Cv^2 * Cr^0  
- cout << "Field(0) [electromagnetic]: " << F0 << endl; 
- auto V0 = Cf / F0;
- cout << "Field(0) variables: " << V0 << endl;
- auto F1 = Cv2 * Cr; // ie: Cv^2 * Cr^1  
- cout << "Field(1) [gravitational]: " << F1 << endl; 
+
+ cout << endl;
+
+ cout << "*** The five fundamental forces of nature? ***" << endl;
+
+ cout << endl;
+  
+ auto F1 = Cv2; // ie: Cv^2 * Cr^0
+ cout << "Field(1) [electromagnetic]: " << F1 << endl;
  auto V1 = Cf / F1;
  cout << "Field(1) variables: " << V1 << endl;
- auto F2 = Cv2 * Cr * Cr; // ie: Cv^2 * Cr^2  
- cout << "Field(2) [strong force?]: " << F2 << endl;
+
+ cout << endl;
+
+ auto F2 = Cv2 * Cr; // ie: Cv^2 * Cr^1
+ cout << "Field(2) [gravitational]: " << F2 << endl;
  auto V2 = Cf / F2;
  cout << "Field(2) variables: " << V2 << endl;
- auto F3 = Cv2 * Cr * Cr * Cr; // ie: Cv^2 * Cr^3  
- cout << "Field(3) [X17 particle?]: " << F3 << endl;
+
+ cout << endl;
+
+ auto F3 = Cv2 * Cr * Cr; // ie: Cv^2 * Cr^2
+ cout << "Field(3) [quark field?]: " << F3 << endl;
  auto V3 = Cf / F3;
  cout << "Field(3) variables: " << V3 << endl;
- cout << "*** Connection between charge and length-mass? ***" << endl;
- auto Pc2 = Pc * Pc;
- cout << "Planck charge squared: " << Pc2 << endl; 
- auto scale = 1e-7;
- auto Pc2_ = Lm / scale;
- cout << "Length-mass with scaling removed: " << Pc2_ << endl;
+
+ cout << endl;
+
+ auto F4 = Cv2 * Cr * Cr * Cr; // ie: Cv^2 * Cr^3
+ cout << "Field(4) [X17 particle?]: " << F4 << endl;
+ auto V4 = Cf / F4;
+ cout << "Field(4) variables: " << V4 << endl;
+
+ cout << endl;
+
+ auto F5 = Cv2 * Cr * Cr * Cr * Cr; // ie: Cv^2 * Cr^4
+ cout << "Field(5) [???]: " << F5 << endl;
+ auto V5 = Cf / F5;
+ cout << "Field(5) variables: " << V5 << endl;
 }
+
